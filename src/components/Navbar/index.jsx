a22c1ffc299891c1a63f940e9c07b67567433549
Daniel Ochoa <danielochoa@MacBook-Pro-de-Daniel.local>
2024-01-24 23:24:11 -0300

    Finishing navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar-container">
+      <div className="navbar-brand">Brand</div>
+      <div className="navbar-categories">Categories</div>
+      <div className="navbar-cart">Cart</div>
+    </nav>
+  );
+};
+
+export default Navbar;
